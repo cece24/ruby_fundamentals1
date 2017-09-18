d91fc576f80167f184021832ae3fecdc7701579a
Cecilia Wong <ceciliakhwong@gmail.com>
2017-09-18 15:26:57 -0400

    Complete exercise 6 part 1, end questions when user goes home

---
+++ exercise6-1.rb
@@ -0,0 +1,17 @@
+total_distance = 0
+done_exercise = false
+
+while done_exercise == false
+  puts "Would you like to walk or run or go home?"
+  user_exercise_type = gets.chomp
+
+  if user_exercise_type == "walk"
+    total_distance += 1
+  elsif user_exercise_type == "run"
+    total_distance += 5
+  elsif user_exercise_type == "go home"
+    done_exercise = true
+  end
+
+  puts "Distance from home is #{total_distance}km."
+end
