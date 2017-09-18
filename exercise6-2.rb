cc3f7801b2b8f972b779bf3804605f7afda1cbba
Cecilia Wong <ceciliakhwong@gmail.com>
2017-09-18 16:03:14 -0400

    Validates user input for energy level

--- exercise6-2.rb
+++ exercise6-2.rb
@@ -1,23 +1,40 @@
 total_distance = 0
 done_exercise = false
+energy_valid = false
 
 puts "How much energy do you have, on a scale of 1 to 30?"
 energy = gets.to_i
+
+while energy_valid == false
+  if energy < 1
+    puts "Invalid entry, please enter in a number between 1 and 30"
+    puts "How much energy do you have, on a scale of 1 to 30?"
+    energy = gets.to_i
+  elsif energy > 30
+    puts "Invalid entry, please enter in a number between 1 and 30"
+    puts "How much energy do you have, on a scale of 1 to 30?"
+    energy = gets.to_i
+  else
+    energy_valid = true
+  end
+end
 
 while done_exercise == false
   if energy <= 0
     done_exercise = true
     puts "You have run out of energy! Time to go home"
   else
-    puts "Would you like to walk or run or go home?"
+    puts "Would you like to walk, run, rest or go home?"
     user_exercise_type = gets.chomp
-    
+
     if user_exercise_type == "walk"
       total_distance += 1
       energy += 5
     elsif user_exercise_type == "run"
       total_distance += 5
       energy -= 5
+    elsif user_exercise_type == "rest"
+      energy += 5
     elsif user_exercise_type == "go home"
       done_exercise = true
     else
