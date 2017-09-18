384be8d249dec7f201f015a1e024d4cbe432fc2f
Cecilia Wong <ceciliakhwong@gmail.com>
2017-09-18 14:03:41 -0400

    Complete exercise 2

---
+++ exercise2.rb
@@ -0,0 +1,7 @@
+puts 55 * 0.2 # calculate tip for 55 dollar meal
+
+puts "meow" + 3.to_s # adding string and integer, convert integer to string
+
+puts "The answer is #{45628 * 7839} which is a pretty big number" # adding code within a string
+
+puts "The results of the expression #{(10 < 20 && 30 < 20) || !(10 == 11)} should be true"
